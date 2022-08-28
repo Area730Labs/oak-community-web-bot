9c41d270b89cbf66ba6be5638d73b367a4e2b377
Vladyslav Melnychenko <webmonch@gmail.com>
2022-08-28 12:05:50 +0200

    initial

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,37 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { SolflareWalletAdapter, PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { clusterApiUrl } from '@solana/web3.js';
+import {useMemo} from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
+require('@solana/wallet-adapter-react-ui/styles.css');
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const network = WalletAdapterNetwork.Mainnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const wallets = useMemo(
+      () => [
+          new SolflareWalletAdapter(),
+          new PhantomWalletAdapter()
+      ],
+      []
+  );
+
+  return (
+      <ChakraProvider>
+        <ConnectionProvider endpoint={endpoint}>
+            <WalletProvider wallets={wallets} >
+                <WalletModalProvider>
+                    <Component {...pageProps} />
+                </WalletModalProvider>
+            </WalletProvider>
+        </ConnectionProvider>
+      </ChakraProvider>
+  )
 }
 
 export default MyApp
